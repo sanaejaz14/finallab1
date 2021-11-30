a909a518313719dc1b64952e9c7210f49d63ef89
sanaejaz14 <95268652+sanaejaz14@users.noreply.github.com>
2021-11-30 01:06:12 -0800

    Add files via upload

---
+++ finallab/finallab/finallab/Employee.cs
@@ -0,0 +1,70 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace finallab
+{
+    public class Employee : HourlyEmployee
+    {
+        private int basepay;
+        private int hourlyPayRate;
+        private int ngh;
+
+        public int Base_Hours { get; private set; }
+
+        public int EmployeeId
+        {
+            get
+            {
+                throw new System.NotImplementedException();
+            }
+
+            set
+            {
+            }
+        }
+
+        public String EmployeeName
+        {
+            get
+            {
+                throw new System.NotImplementedException();
+            }
+
+            set
+            {
+            }
+        }
+
+        public int EmployeeType
+        {
+            get
+            {
+                throw new System.NotImplementedException();
+            }
+
+            set
+            {
+            }
+        }
+
+        public int Hours
+        {
+            get
+            {
+                throw new System.NotImplementedException();
+            }
+
+            set
+            {
+            }
+        }
+
+        public void Salary()
+        {
+            throw new System.NotImplementedException();
+           
+        }
+    }
+}
